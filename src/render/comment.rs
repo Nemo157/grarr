3b82e97522bd972524a19206ba8b6bc44399b527
Wim Looman <wim@nemo157.com>
2016-01-22 18:36:06 +0100

    Add some better layout and styling for request and comments

--- src/render/comment.rs
+++ src/render/comment.rs
@@ -1,32 +1,57 @@
 use git_appraise::{ Comment, Comments };
 use maud_pulldown_cmark::markdown;
+use chrono::naive::datetime::NaiveDateTime;
 
 renderers! {
   CommentsRenderer(comments: Comments) {
-    div {
-      "Comments: "
-      ul {
-        #for comment in comments {
-          li {
-            #(CommentRenderer(comment))
-          }
+    div class="comments" {
+      #for comment in comments {
+        #(CommentRenderer(comment))
+      }
+    }
+  }
+
+  CommentHeaderRendererer(comment: &'a Comment) {
+    div class="block-header comment-header" {
+      h3 {
+        span class="email"
+          #comment.author().unwrap_or("<unknown author>")
+        " commented "
+        #if let Some(timestamp) = comment.timestamp() {
+          "on "
+          span class="timestamp"
+            #(NaiveDateTime::from_timestamp(timestamp.seconds(), 0))
+          " "
+        }
+        "with status "
+        span class={
+          "status "
+          #comment.resolved().map(|r| if r { "lgtm" } else { "nmw" }).unwrap_or("fyi")
+        } {
+          #comment.resolved().map(|r| if r { "lgtm" } else { "nmw" }).unwrap_or("fyi")
+          // #comment.resolved().map(|r| if r { "👍" } else { "👎" }).unwrap_or("ℹ️")
         }
       }
     }
   }
 
+  CommentDetailsRendererer(comment: &'a Comment) {
+    #if let Some(location) = comment.location() {
+      div class="block-details comment-details" {
+        pre { code { #(format!("{:?}", location)) } }
+      }
+    }
+    #if let Some(description) = comment.description() {
+      div class="block-details comment-details" {
+        #(markdown::from_string(description))
+      }
+    }
+  }
+
   CommentRenderer(comment: Comment) {
-    div {
-      #if let Some(author) = comment.author() {
-        div { "Comment from " #author }
-      }
-      div { "Comment Status: " #comment.resolved().map(|r| if r { "lgtm" } else { "nmw" }).unwrap_or("fyi") }
-      #if let Some(location) = comment.location() {
-        div { "Referencing " #(format!("{:?}", location)) }
-      }
-      #if let Some(description) = comment.description() {
-        div { #(markdown::from_string(description)) }
-      }
+    div class="block comment" {
+      #CommentHeaderRendererer(&comment)
+      #CommentDetailsRendererer(&comment)
     }
   }
 }
--- src/render/request.rs
+++ src/render/request.rs
@@ -2,33 +2,50 @@ use git_appraise::Request;
 use maud_pulldown_cmark::markdown;
 use chrono::naive::datetime::NaiveDateTime;
 
+fn summary(request: &Request) -> Option<&str> {
+  request.description()
+    .and_then(|description| description.lines().nth(0))
+}
+
 renderers! {
   RequestRenderer(request: &'a Request) {
-    div class="request" {
+    div class="block request" {
       #RequestHeaderRenderer(request)
       #RequestDetailsRenderer(request)
     }
   }
 
   RequestHeaderRenderer(request: &'a Request) {
-    div class="request-header" {
-      span {
+    div class="block-header request-header" {
+      h4 {
         #if let Some(timestamp) = request.timestamp() {
-          #(NaiveDateTime::from_timestamp(timestamp.seconds(), 0))
-          " "
+          span class="timestamp"
+            #(NaiveDateTime::from_timestamp(timestamp.seconds(), 0))
         }
-        "Merge from "
-        code #request.review_ref().unwrap_or("<unknown ref>")
-        " to "
-        code #request.target_ref().unwrap_or("<unknown ref>")
-        " requested by "
-        #request.requester().unwrap_or("<unknown requester>")
+      }
+      h2 {
+        #if let Some(summary) = summary(request) {
+          #summary
+        }
+        #if let None = summary(request) {
+          "No summary provided"
+        }
+      }
+      h3 {
+        span class="email"
+          #request.requester().unwrap_or("<unknown requester>")
+        " wants to merge "
+        code class="ref"
+          #request.review_ref().unwrap_or("<unknown ref>")
+        " into "
+        code class="ref"
+          #request.target_ref().unwrap_or("<unknown ref>")
       }
     }
   }
 
   RequestDetailsRenderer(request: &'a Request) {
-    div class="request-details" {
+    div class="block-details request-details" {
       #if let Some(description) = request.description() {
         #(markdown::from_string(description))
       }
