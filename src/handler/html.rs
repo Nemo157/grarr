72412cefe41d95f69472925b4432a7ee6bfd5cd8
Wim Looman <wim@nemo157.com>
2016-01-27 19:40:39 +0100

    Fix charset

--- src/handler/html.rs
+++ src/handler/html.rs
@@ -15,7 +15,7 @@ impl<R: RenderOnce> Into<Response> for Html<R> {
 impl<R: RenderOnce> Modifier<Response> for Html<R> {
   fn modify(self, response: &mut Response) {
     let buffer = to_string!(#(self.0));
-    let mime: Mime = "text/html".parse().unwrap();
+    let mime: Mime = "text/html; charset=UTF-8".parse().unwrap();
     (mime, buffer).modify(response)
   }
 }
