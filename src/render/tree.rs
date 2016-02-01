a225df619b4e04c0e8167440f9f04113672f3fd1
Wim Looman <wim@nemo157.com>
2016-02-01 12:32:11 +0100

    Sort tree entries by type

--- src/render/tree.rs
+++ src/render/tree.rs
@@ -1,33 +1,31 @@
 use std::str;
+use std::cmp::{ Ord, PartialOrd, Ordering };
 use std::fmt;
-use super::fa::{ FA };
+use super::fa::{ FA, FAM };
 use git2::{ Tree, TreeEntry, ObjectType, Repository, Blob };
 use std::path::{ Path, PathBuf, Components, Component };
 
 renderers! {
-  TreeEntryStubRenderer(parent: &'a str, entry: &'a TreeEntry<'a>) {
+  TreeEntryStubRenderer(root: &'a str, entry: &'a TreeEntry<'a>) {
     #if let Some(name) = entry.name() {
-      div {
-        a href={ #parent "/" #name } {
-          #{match entry.kind() {
-            Some(ObjectType::Tree) => FA::Sitemap,
-            Some(ObjectType::Blob) => FA::File,
-            _ => FA::Question,
-          } }
-          " "
-          #name
-        }
+      li {
+        #{match entry.kind() {
+          Some(ObjectType::Tree) => FAM::Li(FA::Sitemap),
+          Some(ObjectType::Blob) => FAM::Li(FA::File),
+          _ => FAM::Li(FA::Question),
+        } }
+        a href={ #root "/" #name } { #name }
       }
     }
   }
 
-  TreeEntryRenderer(repo: &'a Repository, parent: &'a str, path: &'a Path, entry: &'a TreeEntry<'a>) {
+  TreeEntryRenderer(repo: &'a Repository, root: &'a str, path: &'a Path, entry: &'a TreeEntry<'a>) {
     div {
       #if let Some(ObjectType::Tree) = entry.kind() {
-        #TreeRenderer(parent, path, entry.to_object(repo).unwrap().as_tree().unwrap())
+        #TreeRenderer(root, path, entry.to_object(repo).unwrap().as_tree().unwrap())
       }
       #if let Some(ObjectType::Blob) = entry.kind() {
-        #BlobRenderer(parent, path, entry.to_object(repo).unwrap().as_blob().unwrap())
+        #BlobRenderer(root, path, entry.to_object(repo).unwrap().as_blob().unwrap())
       }
       #if let Some(ObjectType::Tag) = entry.kind() {
         "Can't render ObjectType::Tag yet"
@@ -44,22 +42,30 @@ renderers! {
     }
   }
 
-  RootTreeRenderer(parent: &'a str, tree: &'a Tree<'a>) {
-    h2 class="path" { #ComponentsRenderer(parent, PathBuf::from("/").components()) }
-    #for entry in tree.iter() {
-      #TreeEntryStubRenderer(parent, &entry)
+  RootTreeRenderer(root: &'a str, tree: &'a Tree<'a>) {
+    h2 class="path" { #ComponentsRenderer(root, PathBuf::from("/").components()) }
+    ul class="fa-ul" {
+      #for entry in tree.iter().collect::<Vec<_>>().tap(|v| v.sort_by_key(|e| Sorter(e.kind()))) {
+        #TreeEntryStubRenderer(root, &entry)
+      }
     }
   }
 
-  TreeRenderer(parent: &'a str, path: &'a Path, tree: &'a Tree<'a>) {
-    h2 class="path" { #ComponentsRenderer(parent, path.components()) }
-    #for entry in tree.iter() {
-      #TreeEntryStubRenderer(&(parent.to_string() + path.to_str().unwrap()), &entry)
+  TreeRenderer(root: &'a str, path: &'a Path, tree: &'a Tree<'a>) {
+    h2 class="path" { #ComponentsRenderer(root, path.components()) }
+    ul class="fa-ul" {
+      li { #(FAM::Li(FA::LevelUp)) a href=#((root.to_string() + path.parent().and_then(|p| p.to_str()).unwrap_or("")).trim_right_matches('/')) ".." }
+      #for entry in tree.iter().collect::<Vec<_>>().tap(|v| v.sort_by_key(|e| Sorter(e.kind()))) {
+        #TreeEntryStubRenderer(&(root.to_string() + path.to_str().unwrap()), &entry)
+      }
     }
   }
 
-  BlobRenderer(parent: &'a str, path: &'a Path, blob: &'a Blob<'a>) {
-    h2 class="path" { #ComponentsRenderer(parent, path.components()) }
+  BlobRenderer(root: &'a str, path: &'a Path, blob: &'a Blob<'a>) {
+    h2 class="path" { #ComponentsRenderer(root, path.components()) }
+    ul class="fa-ul" {
+      li { #(FAM::Li(FA::LevelUp)) a href=#((root.to_string() + path.parent().and_then(|p| p.to_str()).unwrap_or("")).trim_right_matches('/')) ".." }
+    }
     #if blob.is_binary() {
       pre { code { "Binary file" } }
     }
@@ -73,20 +79,59 @@ pub struct ComponentsRenderer<'a>(&'a str, pub Components<'a>);
 
 impl<'a> ::maud::RenderOnce for ComponentsRenderer<'a> {
   fn render_once(self, mut w: &mut fmt::Write) -> fmt::Result {
-    let mut parent = self.0.to_string();
+    let mut root = self.0.to_string();
     for component in self.1 {
       match component {
         Component::RootDir => {
-          try!(html!(w, { a class="path-component" href={ #parent } "<root>" }));
+          try!(html!(w, { a class="path-component" href={ #root } "<root>" }));
         },
         Component::Normal(component) => {
-          try!(html!(w, { "/" a class="path-component" href={ #parent "/" #component.to_str().unwrap() } #component.to_str().unwrap() }));
-          parent = parent + "/" + component.to_str().unwrap();
+          try!(html!(w, { "/" a class="path-component" href={ #root "/" #component.to_str().unwrap() } #component.to_str().unwrap() }));
+          root = root + "/" + component.to_str().unwrap();
         },
         _ => {
         },
       }
     }
     Ok(())
+  }
+}
+
+trait Tapable {
+  fn tap<F: Fn(&mut Self)>(self, f: F) -> Self;
+}
+
+impl<T: Sized> Tapable for T {
+  fn tap<F: Fn(&mut T)>(mut self, f: F) -> T {
+    f(&mut self);
+    self
+  }
+}
+
+#[derive(Eq, PartialEq)]
+struct Sorter(Option<ObjectType>);
+
+impl Ord for Sorter {
+  fn cmp(&self, other: &Self) -> Ordering {
+    match (self.0, other.0) {
+      (x, y) if x == y => Ordering::Equal,
+      (Some(ObjectType::Tree), _) => Ordering::Less,
+      (_, Some(ObjectType::Tree)) => Ordering::Greater,
+      (Some(ObjectType::Tag), _) => Ordering::Less,
+      (_, Some(ObjectType::Tag)) => Ordering::Greater,
+      (Some(ObjectType::Commit), _) => Ordering::Less,
+      (_, Some(ObjectType::Commit)) => Ordering::Greater,
+      (Some(ObjectType::Blob), _) => Ordering::Less,
+      (_, Some(ObjectType::Blob)) => Ordering::Greater,
+      (Some(ObjectType::Any), _) => Ordering::Less,
+      (_, Some(ObjectType::Any)) => Ordering::Greater,
+      (None, None) => Ordering::Equal,
+    }
+  }
+}
+
+impl PartialOrd for Sorter {
+  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+    Some(self.cmp(other))
   }
 }
