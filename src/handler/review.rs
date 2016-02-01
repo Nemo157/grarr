270497fabecdcad6e4e75900e6400839d7fbb546
Wim Looman <wim@nemo157.com>
2016-02-01 15:37:41 +0100

    Wrap review in repository wrapper

--- src/handler/review.rs
+++ src/handler/review.rs
@@ -1,5 +1,6 @@
 use super::base::*;
 
+use std::fs;
 use std::path::PathBuf;
 use router::Router;
 use git_appraise::{ Oid, Repository };
@@ -13,9 +14,10 @@ impl Handler for Review {
   fn handle(&self, req: &mut Request) -> IronResult<Response> {
     let path = req.extensions.get::<Router>().unwrap().find("repo").unwrap();
     let repo = Repository::open(self.root.join(path)).unwrap();
+    let actual = fs::canonicalize(self.root.join(path)).unwrap().strip_prefix(&fs::canonicalize(&self.root).unwrap()).unwrap().to_str().unwrap().to_string();
     let id = Oid::from_str(req.extensions.get::<Router>().unwrap().find("commit_id").unwrap()).unwrap();
     let review = repo.review_for(id).unwrap();
-    Ok(Html(Wrapper(&ReviewRenderer(&review))).into())
+    Ok(Html(Wrapper(RepositoryWrapper(&*path, &actual, Tab::Reviews, &ReviewRenderer(&review)))).into())
   }
 }
 
