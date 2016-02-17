a00feeed1835d231ff1469e2f1b439cffdb67d19
Wim Looman <wim@nemo157.com>
2016-02-17 17:44:23 +0100

    Use commit on context for commits handler

--- src/handler/commits.rs
+++ src/handler/commits.rs
@@ -7,14 +7,12 @@ pub struct Commits;
 
 impl Handler for Commits {
   fn handle(&self, req: &mut Request) -> IronResult<Response> {
-    let router = itry!(req.extensions.get::<Router>().ok_or(Error::MissingExtension), status::InternalServerError);
     let context = itry!(req.extensions.get::<RepositoryContext>().ok_or(Error::MissingExtension), status::InternalServerError);
-    let reff = itry!(router.find("ref").ok_or(Error::MissingPathComponent), status::InternalServerError);
-    let object = itry!(context.repository.revparse_single(reff), status::NotFound);
-    let commit = itry!(object.as_commit().ok_or(Error::String("Object is not commit...")), status::InternalServerError);
+    let commit = itry!(context.commit(), status::InternalServerError);
+    let reference = context.reference().ok().and_then(|r| r.shorthand().map(ToOwned::to_owned)).unwrap_or_else(|| format!("{}", commit.id()));
     let commits = itry!(CommitTree::new(&context.repository, &commit), status::InternalServerError);
     Html {
-      render: Wrapper(RepositoryWrapper(&context, render::Commits(&("/".to_owned() + context.requested_path.to_str().unwrap()), &reff, commits))),
+      render: Wrapper(RepositoryWrapper(&context, render::Commits(&("/".to_owned() + context.requested_path.to_str().unwrap()), &reference, commits))),
       etag: Some(EntityTag::weak(versioned_sha1!(commit.id().as_bytes()))),
       req: req,
     }.into()
